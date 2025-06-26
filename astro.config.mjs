4651c9afc450a7942b4f81aa4573dfd9cfde6eb0
MohMost <teemohmost2020@gmail.com>
2025-06-26 09:02:37 +0100

    🪲(fix) : upgrade astro to v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "ar",
